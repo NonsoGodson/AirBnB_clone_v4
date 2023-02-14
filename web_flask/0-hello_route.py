c3f35cf1de62c900388c308c88cb82f6ca1a43c9
NonsoGodson <onyedumekwunonso@yahoo.com>
2023-02-14 18:34:28 +0000

    commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,14 @@
-#!/usr/bin/python3
-'''A simple Flask web application.
-'''
+tarts a Flask web application
+"""
 from flask import Flask
 
 
 app = Flask(__name__)
-'''The Flask application instance.'''
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index():
-    '''The home page.'''
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 
